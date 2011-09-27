b22932a9365fd1e921fddc3b2ea75c8887b0facf
Andreas Sandberg <andreas@sandberg.pp.se>
2011-09-27 18:29:22 +0200

    Fix RUN_BENCH so it actually calls func rather than bench_iteration

--- lib/include/bench_common.h
+++ lib/include/bench_common.h
@@ -57,7 +57,7 @@
 	    }								\
 	} else {							\
 	    while (1) {							\
-		bench_iteration();					\
+		func();                                                 \
 	    }								\
 	}								\
 	cycles_stop = cycles_get();					\
